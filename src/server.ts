eceb527575c6143d4cfbbd53f6fd9fedfa11a9fd
iteshxt <petrioteer007@gmail.com>
2026-05-14 04:16:45 +0530

    removed obsolete server-side rendering and cloudflare config files

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createStartHandler } from "@tanstack/start/server";
-import { getRouter } from "./router";
-import { startInstance } from "./start";
-
-export default createStartHandler({
-  createRouter: getRouter,
-  getStartInstance: () => startInstance,
-});
