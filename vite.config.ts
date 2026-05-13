d328123406acd9b33564659bbe6d225cf75dc785
iteshxt <petrioteer007@gmail.com>
2026-05-14 04:12:06 +0530

    refactored frontend for vercel native deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    build: {
-      ssr: false,
-    },
-  },
   tanstackStart: {
-    server: { entry: "server" },
+    server: { entry: "src/server.ts" },
   },
 });
